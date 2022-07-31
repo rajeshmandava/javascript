f175613ddc90147d4669d1563dcef32e864f1e04
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-07-31 22:36:45 +0200

    values and variables

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ console.log("Rajesh Mandava");
 let firstName = "Rajesh";
 let secondName = "Mandava";
 console.log(firstName + secondName);
+let isLand = false;
+let country = "India";
+let continent = "Asia";
+let population = "1.38 Billion";
+
+console.log(
+  "Country is " +
+    country +
+    " in continent " +
+    continent +
+    " with a population of " +
+    population
+);
+console.log(isLand);
