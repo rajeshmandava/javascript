6544ddfa336c2f09174cd8cfdbeb8dfad6a77c52
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-01 12:55:16 +0200

    using variables, operators in javascript

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Rajesh Mandava");
+// console.log("Rajesh Mandava");
 let firstName = "Rajesh";
 let secondName = "Mandava";
-console.log(firstName + secondName);
+// console.log(firstName + secondName);
 let isLand = false;
 let country = "India";
 let continent = "Asia";
 let population = "1.38 Billion";
 
-console.log(
-  "Country is " +
-    country +
-    " in continent " +
-    continent +
-    " with a population of " +
-    population
-);
-console.log(isLand);
+// console.log(
+//   "Country is " +
+//     country +
+//     " in continent " +
+//     continent +
+//     " with a population of " +
+//     population
+// );
+// console.log(isLand);
+let markWeight = 78,
+  markHeight = 1.69;
+let johnWeight = 92,
+  johnHeight = 1.95;
+
+let markBMI = markWeight / (markHeight * markHeight);
+let johnBMI = johnWeight / (johnHeight * johnHeight);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
