e53ab0b951d0baf675caf231c1bb73df0e2a3cce
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-05 23:48:55 +0200

    Implementing the game logic

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -7,11 +7,49 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  console.log(guess, secretNumber);
   if (!guess) {
     document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!!!";
+    document.querySelector(".number").textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😞 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😞 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start Guessing ...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").textContent = " ";
+  console.log(secretNumber);
 });
