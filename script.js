f955bad6c2e848cf9b434a4d680b1c79fef95dcd
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-05 21:54:51 +0200

    Selecting and manipulating Elements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+console.log(document.querySelector(".message"));
+document.querySelector(".message").textContent = "Correct Number🎉";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
