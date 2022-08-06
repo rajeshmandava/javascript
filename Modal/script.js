277ac56717d7cdf3bc2c27a7517eb5c1c7098808
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-06 11:25:13 +0200

    Add show and hide functionalities

--- Modal/script.js
+++ Modal/script.js
@@ -5,7 +5,22 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+const showModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
 console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  console.log(btnsOpenModal[i].addEventListener("click", showModal));
 }
+
+const hideModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", hideModal);
+
+overlay.addEventListener("click", hideModal);
