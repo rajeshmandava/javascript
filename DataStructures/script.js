a56cd3ab5d92b3abb98bc4085a0065d6a6dc431d
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-07 22:34:44 +0200

    DataStructures and Operators

---
+++ DataStructures/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegatarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pista", "Risotto"],
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
