f5671b1828482ad27f8734a82823faefc79482e4
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-15 23:57:52 +0200

    Built-in length property and ex3.1

--- JavaScipt Multiple Values/script.js
+++ JavaScipt Multiple Values/script.js
@@ -20,4 +20,56 @@ const arr5 = ["hi there"];
 arr5[0] = "new value";
 console.log(arr5[0]);
 
-arr5 = ["Changing the const full array value"];
+// arr5 = ["Changing the const full array value"];
+
+// Accessing Elements
+let cars = ["Porsche", "BMW", "Mercedes", "Volkswagen", "McLaren"];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]);
+console.log(cars[4]);
+
+// Negative index values
+console.log(cars[-1]);
+console.log(cars[-2]);
+console.log(cars[-3]);
+// We didn't a value to negative indexes in arrays So, when requested values for negative indexes, the returned value is undefined
+
+//Overwriting Elements
+cars[0] = "Tesla";
+cars[1] = "Volvo";
+cars[2] = "Polestar";
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+cars[-1] = "Kia";
+cars[-2] = "Toyota";
+cars[-3] = "Hyundai";
+console.log(cars[-1]);
+console.log(cars[-2]);
+console.log(cars[-3]);
+
+//Built-in length property
+let colors = ["purple", "white", "Red", "Blue", "Green"];
+let boolean = [true, false, true, true, false, true, false, false, false];
+let emptyArray = [];
+
+console.log(`Length of colors array:${colors.length}`);
+console.log(`length of boolean array:${boolean.length}`);
+console.log(`length of emptyArray:${emptyArray.length}`);
+
+console.log(`last element of boolean array is ${boolean[boolean.length - 1]}`);
+console.log(`last element of colors array is ${colors[colors.length - 1]}`);
+
+let numbers = [12, 24, 36];
+numbers[5] = 72;
+console.log(numbers);
+
+// Practice exercise 3.1
+let shoppingList = ["Milk", "Bread", "Apples"];
+console.log(`length of shoppingList array is ${shoppingList.length}`);
+shoppingList[1] = "Bananas";
+console.log(shoppingList);
